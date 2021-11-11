e1c975c364c7cd0329406d2c2eb124ab9c0966cb
Victor Noël <victor.noel@crazydwarves.org>
2021-11-11 18:31:25 +0100

    Switch to Github Actions

---
+++ .github/maven.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec mvn -B -V -e -U "$@"
--- .travis/prepare-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_5c15cf265382_key -iv $encrypted_5c15cf265382_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-gpg --fast-import .travis/codesigning.asc
